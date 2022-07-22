826ab0c3f0306322520a81203268e2b2750ffb7b
BowersIndustry <83834271+BowersIndustry@users.noreply.github.com>
2022-07-22 08:36:59 -0500

    Did some organization
    Organized the project. Now to work on the searching

--- js/index/app.js
+++ js/index/app.js
@@ -1,3 +1,8 @@
+import verses from '../../node_modules/kjv/json/verses-1769.json';
+verses = JSON.stringify(verses);
+ 
+console.log(verses['John 3:16'])
+
 const hamburger_menu = document.querySelector(".hamburger-menu");
 const container = document.querySelector(".container");
 const ShareVerseBtn = document.getElementById("ShareBtn");
@@ -12,6 +17,15 @@ ShareVerseBtn.addEventListener("click", () => {
 
 	navigator.clipboard.writeText(VerseNamePlusText);
 });
+
+const get_data = async (book, chapter, verse, version) => {
+    let res = await fetch(`https://jsonbible.com/search/verses.php?json={"book":${book}, "chapter":${chapter}, "verse":${verse}, "version":${version}}`)
+    console.log(res.json())
+    return res.json()
+
+}   
+
+var chips = get_data("John", "3", "16", "kjv");
 
 hamburger_menu.addEventListener("click", () => { container.classList.toggle("active"); });
 
