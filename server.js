491a7935445ea7e302efae807c2059f0e2d8f4cd
BowersIndustry <Bowersindustry.bwrs@gmail.com>
2022-08-31 21:18:20 -0500

    Loads index.html
    Now the express loads index.html but its not loading any css and I have no idea why

--- server.js
+++ server.js
@@ -1,4 +1,13 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const path = require('path');
 
-app.listen(5500/login)
+const app = express();
+const port = process.env.PORT || 8080;
+
+// sendFile will go here
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.listen(port);
+console.log('Server started at http://localhost:' + port);
