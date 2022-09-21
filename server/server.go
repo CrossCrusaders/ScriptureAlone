9f0c465139b55fe1ae56d8b3b46b31971c3c746e
Tyler <tyler.thomas.127@gmail.com>
2022-09-21 12:24:15 -0500

    add bible api endpoints

--- server/server.go
+++ server/server.go
@@ -4,6 +4,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 
 	"github.com/joho/godotenv"
@@ -22,22 +23,158 @@ func goDotEnvVariable(key string) string {
 }
 
 func main() {
-	val := goDotEnvVariable("BIBLE_API_KEY")
-	log.Print(val)
+	bibleApiKey := goDotEnvVariable("BIBLE_API_KEY")
+	log.Print(bibleApiKey)
 	app := pocketbase.New()
 
+	//search
 	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
 		// add new "GET /api/hello" route to the app router (echo)
 		e.Router.AddRoute(echo.Route{
 			Method: http.MethodGet,
-			Path:   "/api/bible-search",
+			Path:   "/api/bibles/search",
 			Handler: func(c echo.Context) error {
-				resp, err := http.Get(`https://randomuser.me/api/`)
+				bibleId := c.QueryParam(`bible_id`)
+				query := c.QueryParam(`q`)
+				offset := c.QueryParam(`offset`)
 
+				url := `https://api.scripture.api.bible/v1/bibles/` + bibleId + `/search` + `?query=` + url.QueryEscape(query)
+
+				if offset != `` {
+					url += `&offset=` + offset
+				}
+
+				client := http.Client{}
+				req, _ := http.NewRequest("GET", url, nil)
+
+				req.Header = http.Header{
+					"api-key":      {bibleApiKey},
+					"Content-Type": {"application/json"},
+				}
+				resp, err := client.Do(req)
 				if err != nil {
-					log.Fatal(`Error calling bible search api`)
+					log.Fatal(err)
 					return c.String(500, err.Error())
 				}
+
+				body, err := ioutil.ReadAll(resp.Body)
+
+				if err != nil {
+					log.Fatal(err)
+					return c.String(500, err.Error())
+				}
+
+				return c.String(200, string(body))
+			},
+			Middlewares: []echo.MiddlewareFunc{},
+		})
+
+		return nil
+	})
+
+	// Bibles search
+	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+		e.Router.AddRoute(echo.Route{
+			Method: http.MethodGet,
+			Path:   "/api/bibles",
+			Handler: func(c echo.Context) error {
+				ids := c.QueryParam(`bible_ids`)
+				url := `https://api.scripture.api.bible/v1/bibles?language=eng`
+				if ids != `` {
+					url += `&ids=` + ids
+				}
+
+				client := http.Client{}
+				req, _ := http.NewRequest("GET", url, nil)
+
+				req.Header = http.Header{
+					"api-key":      {bibleApiKey},
+					"Content-Type": {"application/json"},
+				}
+				resp, _ := client.Do(req)
+
+				body, err := ioutil.ReadAll(resp.Body)
+
+				if err != nil {
+					log.Fatal(`Error parsing bible search response`)
+					return c.String(500, err.Error())
+				}
+
+				return c.String(200, string(body))
+			},
+			Middlewares: []echo.MiddlewareFunc{},
+		})
+
+		return nil
+	})
+
+	// List books
+	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+		e.Router.AddRoute(echo.Route{
+			Method: http.MethodGet,
+			Path:   "/api/bibles/books",
+			Handler: func(c echo.Context) error {
+				bibleId := c.QueryParam(`bible_id`)
+				bookId := c.QueryParam(`book_id`)
+
+				url := `https://api.scripture.api.bible/v1/bibles/` + bibleId + `/books`
+
+				if bookId != `` {
+					url += `/` + bookId
+				}
+				url += `?include-chapters=true`
+				log.Print(url)
+
+				client := http.Client{}
+				req, _ := http.NewRequest("GET", url, nil)
+
+				req.Header = http.Header{
+					"api-key":      {bibleApiKey},
+					"Content-Type": {"application/json"},
+				}
+				resp, _ := client.Do(req)
+
+				body, err := ioutil.ReadAll(resp.Body)
+
+				if err != nil {
+					log.Fatal(`Error parsing bible search response`)
+					return c.String(500, err.Error())
+				}
+
+				return c.String(200, string(body))
+			},
+			Middlewares: []echo.MiddlewareFunc{},
+		})
+
+		return nil
+	})
+
+	// Get specific chapter
+	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+		e.Router.AddRoute(echo.Route{
+			Method: http.MethodGet,
+			Path:   "/api/bibles/chapters",
+			Handler: func(c echo.Context) error {
+				bibleId := c.QueryParam(`bible_id`)
+				chapterId := c.QueryParam(`chapter_id`)
+
+				url := `https://api.scripture.api.bible/v1/bibles/` + bibleId + `/chapters`
+
+				if chapterId != `` {
+					url += `/` + chapterId
+					url += `?content-type=html&include-notes=false&include-titles=true&include-chapter-numbers=false&include-verse-numbers=true&include-verse-spans=false`
+				}
+
+				log.Print(url)
+
+				client := http.Client{}
+				req, _ := http.NewRequest("GET", url, nil)
+
+				req.Header = http.Header{
+					"api-key":      {bibleApiKey},
+					"Content-Type": {"application/json"},
+				}
+				resp, _ := client.Do(req)
 
 				body, err := ioutil.ReadAll(resp.Body)
 
