8b1e2d16fbd4af39b5705592bdaf61054b0936c2
Tyler <tyler.thomas.127@gmail.com>
2022-09-22 10:05:48 -0500

    add fade transition to menu

--- src/components/molecules/TopNavMenu.vue
+++ src/components/molecules/TopNavMenu.vue
@@ -1,18 +1,20 @@
 <template>
   <nav v-if="breakpoint === 'sm'">
     <span class="material-icons-outlined" @click="mobileMenuOpen = true">menu</span>
-    <div class="fixed bg-gray-800 z-50 text-white left-0 right-0 top-0 bottom-0" v-if="showMobileMenuOpen">
-	  <span class="material-icons-outlined absolute right-2 top-2" @click="mobileMenuOpen = false">close</span>
-	  <div class="p-2" v-for="item in menuItems" :key="item.label">
-	    {{ item.label }}
-		<div v-if="item.children && item.children.length">
-		  <hr>
-		  <div class="p-2 pl-4" v-for="child in item.children" :key="child.label">
-            <a :href="child.link || '#'">{{ child.label }}</a>
+    <Transition>
+      <div class="top-nav-menu__mobile-menu-panel transition-all bg-gray-800 z-50 text-white" v-if="showMobileMenuOpen">
+        <span class="material-icons-outlined absolute right-2 top-2" @click="mobileMenuOpen = false">close</span>
+        <div class="p-2" v-for="item in menuItems" :key="item.label">
+          {{ item.label }}
+          <div v-if="item.children && item.children.length">
+            <hr>
+            <div class="p-2 pl-4" v-for="child in item.children" :key="child.label">
+              <a :href="child.link || '#'">{{ child.label }}</a>
+            </div>
           </div>
-		</div>
-	  </div>
-    </div>
+        </div>
+      </div>
+    </Transition>
   </nav>
   <nav v-else>
     <ul class="flex flex-row gap-4 items-center">
@@ -21,9 +23,11 @@
           <span class="underline">{{ item.label }}</span>
           <span v-if="item.children && item.children.length" class="material-icons-outlined">expand_more</span>
         </a>
-        <ul class="top-nav-submenu p-4 bg-gray-800 text-white min-w-fit  shadow-lg rounded" v-if="item.children && item.children.length">
+        <ul class="top-nav-submenu p-4 bg-gray-800 text-white min-w-fit  shadow-lg rounded"
+          v-if="item.children && item.children.length">
           <li v-for="child in item.children" :key="child.label">
-            <a class="font-bold underline whitespace-nowrap text-center w-full" :href="child.link || '#'">{{ child.label }}</a>
+            <a class="font-bold underline whitespace-nowrap text-center w-full" :href="child.link || '#'">{{ child.label
+            }}</a>
           </li>
         </ul>
       </li>
@@ -70,18 +74,40 @@ const showMobileMenuOpen = computed(() => {
   return mobileMenuOpen.value && breakpoint.value === 'sm'
 })
 
+
 </script>
 
 <style lang="css" scoped>
-  .top-nav-menu-item {
-    position: relative;
-  }
-  .top-nav-menu-item > ul {
-    display: none;
-    position: absolute;
+.top-nav-menu-item {
+  position: relative;
+}
 
-  }
-  .top-nav-menu-item:hover > ul{
-    display: block;
-  }
+.top-nav-menu-item>ul {
+  display: none;
+  position: absolute;
+
+}
+
+.top-nav-menu-item:hover>ul {
+  display: block;
+}
+
+.top-nav-menu__mobile-menu-panel {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  opacity: 1;
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 150ms ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
 </style>
