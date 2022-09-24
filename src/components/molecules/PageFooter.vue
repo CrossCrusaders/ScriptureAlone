1b5d2080172f81de5148e2d94a67c67aff67dc18
BowersIndustry <Bowersindustry.bwrs@gmail.com>
2022-09-23 20:12:49 -0500

    Fix footer

--- src/components/molecules/PageFooter.vue
+++ src/components/molecules/PageFooter.vue
@@ -9,7 +9,7 @@
           · <a href="/updates.html">Updates</a> · <a href="/contact.html">Contact</a></p>
         <p class="text-xs">Scripture Alone © 2022</p>
       </div>
-      <div class="footerSection md:pl-5 lg:pl-20">
+      <div class="footerSection lg:pl-40">
         <p><i class="material-icons-outlined pr-1">mail</i>
           <a class="mb-2 absolute" href="mailto:support@crosscrusaders.com">support@crosscrusaders.com</a>
         </p>
