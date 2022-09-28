6aae2c58c4fb70dcc63222eb19737197110738b3
BowersIndustry <Bowersindustry.bwrs@gmail.com>
2022-09-28 13:33:55 -0500

    Doing Stuff

--- src/components/molecules/IconCallToAction.vue
+++ src/components/molecules/IconCallToAction.vue
@@ -1,9 +1,16 @@
 <template>
-    <div class="relative bg-[#d9d9d9] p-8 rounded-3xl">
-      <slot class="text-[#959595]" name="title"></slot>
-      <slot name="image"></slot>
+    <div class="flex bg-slate-200 p-8 rounded-3xl" style="flex-direction: row; justify-content:left;">
+      <div class="mr-4 outline">
+        <slot name="image"></slot>
+        sjdk
+      </div>
+      <div class="outline">
+        <slot class="text-[#959595]" name="title"></slot>
+        <slot name="dis"></slot>
+      </div>
+      <div class="flex justify-end w-1/2 outline ml-4">
         <slot name="button"></slot>
-      <slot name="dis">ETtssudbsjf jsfgh. YS fifhh. Hisdhikdfjh udfhjdfis uhsgbd uhfb ifh euhf suhdujief ifncbeu.</slot>
+      </div>
     </div>
   </template>
   
