185cfea81d0e5dcc842f222611a9b4e308031e32
BowersIndustry <Bowersindustry.bwrs@gmail.com>
2022-10-01 16:25:43 -0500

    hacked up code 👍

--- src/devotionals/components/AuthorPreviewColumn.vue
+++ src/devotionals/components/AuthorPreviewColumn.vue
@@ -1,5 +1,5 @@
 <template>
-  <img :src="props.devoObject?.author?.profileImage" class="object-cover max-h-40 w-full md:max-h-64 rounded-lg mb-4" />
+  <img :src="imgURL" class="object-cover max-h-40 w-full md:max-h-64 rounded-lg mb-4" />
   <div class="mb-8">
     <p class="font-bold text-slate-800 mb-2">Speaker: {{ props.devoObject?.author?.firstName + ' ' +
     props.devoObject?.author?.lastName }}</p>
@@ -15,6 +15,7 @@
 </template>
 
 <script setup lang="ts">
+import PocketBaseClient from '../../api/PocketBaseClient'
 import { Author } from '../../authors/Author'
 import { formatAddress } from '../../core/services/FormatService'
 import { Devotional } from '../Devotional';
@@ -26,6 +27,9 @@ const props = defineProps<AuthorPreviewColumnProps>()
 
   console.log(props)
 
-console.log(props.devoObject?.coverImage)
+var imgURL = PocketBaseClient.records.getFileUrl(props.devoObject, props.devoObject?.coverImage, {});
+
+
+console.log(imgURL)
 
 </script>
