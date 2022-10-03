38846c0383e4e4effddf50a7d03f02dda423485f
BowersIndustry <Bowersindustry.bwrs@gmail.com>
2022-10-03 11:28:49 -0500

    yay

--- src/devotionals/components/Accordion.vue
+++ src/devotionals/components/Accordion.vue
@@ -1,74 +1,59 @@
+<script lang="ts" setup>
+  import { reactive } from 'vue'
+
+  let contents = reactive({ Item1Expanded:false, Item2Expanded:false, Item3Expanded:false });
+</script>
+
 <template>
   <div>
-    <div class="title"
-      @click="if(contents[0].expanded != true) contents.forEach(c => { c.expanded = false }); contents[0].expanded = !contents[0].expanded;">
+    <div class="title" @click="contents.Item2Expanded = false; contents.Item3Expanded = false; contents.Item1Expanded = !contents.Item1Expanded;">
       <div>
-        <b>
-          <slot name="Item1Title"></slot>
-        </b>
+        <b><slot name="Item1Title"></slot></b>
       </div>
       <div>
-        <span v-if="contents[0].expanded">&#x2191;</span>
+        <span v-if="contents.Item1Expanded">&#x2191;</span>
         <span v-else>&#x2193;</span>
       </div>
     </div>
-    <div class="description" v-if="contents[0].expanded">
+    <div class="description" v-if="contents.Item1Expanded">
       <slot name="Item1Cont"></slot>
     </div>
-    <div class="title"
-      @click="if(contents[1].expanded != true) contents.forEach(c => { c.expanded = false }); contents[1].expanded = !contents[1].expanded;">
+    <div class="title" @click="contents.Item1Expanded = false; contents.Item3Expanded = false; contents.Item2Expanded = !contents.Item2Expanded;">
       <div>
-        <b>
-          <slot name="Item2Title"></slot>
-        </b>
+        <b><slot name="Item2Title"></slot></b>
       </div>
       <div>
-        <span v-if="contents[1].expanded">&#x2191;</span>
+        <span v-if="contents.Item2Expanded">&#x2191;</span>
         <span v-else>&#x2193;</span>
       </div>
     </div>
-    <div class="description" v-if="contents[1].expanded">
+    <div class="description" v-if="contents.Item2Expanded">
       <slot name="Item2Cont"></slot>
     </div>
-    <div class="title"
-      @click="if(contents[2].expanded != true) contents.forEach(c => { c.expanded = false }); contents[2].expanded = !contents[2].expanded;">
+    <div class="title" @click="contents.Item1Expanded = false; contents.Item2Expanded = false; contents.Item3Expanded = !contents.Item3Expanded;">
       <div>
-        <b>
-          <slot name="Item3Title"></slot>
-        </b>
+        <b><slot name="Item3Title"></slot></b>
       </div>
       <div>
-        <span v-if="contents[2].expanded">&#x2191;</span>
+        <span v-if="contents.Item3Expanded">&#x2191;</span>
         <span v-else>&#x2193;</span>
       </div>
     </div>
-    <div class="description" v-if="contents[2].expanded">
+    <div class="description" v-if="contents.Item3Expanded">
       <slot name="Item3Cont"></slot>
     </div>
   </div>
 </template>
 
-<script setup lang="ts">
-var contents = Array(3)
-  .fill(null)
-  .map((_, i) => {
-    return {
-      expanded: false,
-    };
-  });
-
-</script>
-
 <style scoped>
-.title {
-  cursor: pointer;
-  display: flex;
-  justify-content: space-between;
-}
-
-.title,
-.description {
-  border: 1px solid lightgray;
-  padding: 5px;
-}
+  .title {
+    cursor: pointer;
+    display: flex;
+    justify-content: space-between;
+  }
+  .title,
+  .description {
+    border: 1px solid lightgray;
+    padding: 5px;
+  }
 </style>
