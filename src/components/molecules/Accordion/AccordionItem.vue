b0b373b93854db1523bbdf5714eaf56b9a83549d
BowersIndustry <Bowersindustry.bwrs@gmail.com>
2022-10-04 16:13:18 -0500

    ok

--- src/components/molecules/Accordion/AccordionItem.vue
+++ src/components/molecules/Accordion/AccordionItem.vue
@@ -143,6 +143,5 @@ async function onLeave(el: HTMLElement, done: Function) {
   cursor: pointer;
   background-color: whitesmoke;
   padding: 0.8em;
-  border-bottom: 1px solid lightgray;
 }
 </style>
