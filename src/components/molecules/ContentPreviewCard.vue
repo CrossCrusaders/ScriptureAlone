19ff65c3f05c8e7ee59e5d2d60a6408824f96279
Tyler <tyler.thomas.127@gmail.com>
2022-10-11 09:04:03 -0500

    card component

--- src/components/molecules/ContentPreviewCard.vue
+++ src/components/molecules/ContentPreviewCard.vue
@@ -1,27 +1,28 @@
 <template>
-  <div class="content-preview-card flex flex-col rounded-2xl  bg-slate-200 relative overflow-hidden">
+  <div class="content-preview-card flex flex-col rounded-2xl relative  bg-transparent overflow-hidden">
 
-    <img :src="previewImage" class="w-full h-32 object-cover " />
+    <img :src="previewImage" class="object-cover h-full w-full absolute top-0 left-0 right-0 bottom-0 -z-20 " />
+    <div class="absolute top-0 left-0 right-0 bottom-0 -z-20 h-full w-full bg-slate-800 opacity-75 "></div>
     <div class="flex flex-col p-6 flex-auto">
 
-      <h3 class="text-slate-800 text-xl font-title font-bold mb-0" :title="props.content?.title">{{
+      <h3 class="text-white text-xl font-title font-bold mb-0" :title="props.content?.title">{{
       formatMaxLengthText(props.content?.title || '', 32) }}
       </h3>
-      <p class="text-slate-700 text-sm font-body mb-2">{{ props.content?.author?.firstName }}&nbsp;{{
+      <p class="text-slate-100 text-sm font-body mb-2">{{ props.content?.author?.firstName }}&nbsp;{{
       props.content?.author?.lastName
       }}
       </p>
-      <p class="text-slate-700 text-md font-body mb-3 break-words" :title="props.content?.description">{{
+      <p class="text-slate-100 text-md font-body mb-3 break-words" :title="props.content?.description">{{
       formatMaxLengthText(props.content?.description ||
       '', 48) }}
       </p>
-      <p class="text-slate-600 text-md text-sm font-body mb-0">
+      <p class="text-slate-200 text-md text-sm font-body mb-0">
         Tags:
         <span v-for="(category, innerIndex) in props.content?.categories">
           {{ category.label }},&nbsp;
         </span>
       </p>
-      <p v-if="props.content?.duration" class="text-slate-600 text-md font-body text-sm mb-3">
+      <p v-if="props.content?.duration" class="text-slate-200 text-md font-body text-sm mb-3">
         Duration: {{ formatMillisecondsAsReadableDuration(props.content?.duration) }}
       </p>
       <div class="flex-auto mb-2"></div>
