f03000af2ad80ca175ab1f5743a0e3c9bc6cfcb9
BowersIndustry <Bowersindustry.bwrs@gmail.com>
2022-11-03 15:26:30 -0500

    Update TruthResourcesList.vue

--- src/truthResources/components/TruthResourcesList.vue
+++ src/truthResources/components/TruthResourcesList.vue
@@ -2,7 +2,7 @@
     <h2 class="text-2xl font-bold mb-2 text-slate-900">Text Material:</h2>
     <div class="flex justify-center pb-8">
         <div class="w-1/6 flex flex-col">
-            <label v-for="resource of props.resources"><RouterLink :to="resource.link || '/'"><Icon icon-name="cross" :size="8"></Icon> {{ resource.title }}</RouterLink></label>
+            <RouterLink v-for="resource of props.resources" :to="resource.link || '/'"><Icon icon-name="cross" :size="8"></Icon> {{ resource.title }}</RouterLink>
         </div>
         <div class="pl-4 pr-4 flex flex-row items-center justify-center">
             <div class="h-full w-1 bg-slate-400"></div>
@@ -16,7 +16,8 @@
 </template>
 
 <script setup lang="ts">
-import Icon from '../../components/atoms/Icon.vue'
+import { computed } from 'vue';
+import Icon from '../../components/atoms/Icon.vue';
 import { RouterLink } from 'vue-router';
 
 export interface Resource {
