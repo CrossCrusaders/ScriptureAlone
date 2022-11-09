848870b585e0345edf5001f6b51667a5d51d4cc5
BowersIndustry <Bowersindustry.bwrs@gmail.com>
2022-11-09 13:20:11 -0600

    Update TruthResourceService.ts

--- src/truthResources/services/TruthResourceService.ts
+++ src/truthResources/services/TruthResourceService.ts
@@ -4,4 +4,4 @@ import { transformTruthResourceResponse } from '../TruthResource'
 export const getTruthResource = async (id: string) => {
     const response = await PocketBaseClient.records.getOne('truthResources', id, { expand: 'title,description,author,author.church' })
     return transformTruthResourceResponse(response)
-  }
+}
