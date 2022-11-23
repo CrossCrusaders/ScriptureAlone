40da99e5ad534aed372e740e2e726fb6653074ae
BowersIndustry <Bowersindustry.bwrs@gmail.com>
2022-11-22 21:06:08 -0600

    yes

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
 import { createApp } from 'vue'
 import './style.css'
-import App from './App.vue'
+import VueApp from './App.vue'
 import router from './router'
 import vfmPlugin from 'vue-final-modal'
 import VuePdf from 'vue3-pdfjs'
 
-const app = createApp(App)
+const app = createApp(VueApp)
 app.use(vfmPlugin)
 app.use(router)
 app.use(VuePdf)
 app.mount('#app')
+
+import { App } from '@capacitor/app';
+
+App.addListener('backButton', data => {
+    window.history.back()
+})
